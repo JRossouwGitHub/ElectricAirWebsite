8ba6a81ef452fe29cb2383d7b150d88421656559
Jacques Rossouw <jrossouw.work@gmail.com>
2021-11-08 10:50:31 +1300

    Products

--- pages/products.js
+++ pages/products.js
@@ -27,80 +27,83 @@ const products = () => {
         <div className={styles.root}>
             <Meta title={'Products'} />
             <Tabs active={[false, false, true, false]} />
-            <div className={isMobile ? stylesProducts.listM : stylesProducts.list}>
-                <List
-                    sx={{ width: '100%', bgcolor: 'background.paper' }}
-                    component="nav"
-                    aria-labelledby="nested-list-subheader"
-                    >
-                    <ListItemButton onClick={handleClick} className={stylesProducts.listItem}>
-                        <ListItemText>
-                            <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Air Compressors</span>
-                        </ListItemText>
-                        {open ? <ExpandLess /> : <ExpandMore />}
-                    </ListItemButton>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <List component="div" disablePadding>
-                            <Link href='/rotary'>
-                                <ListItemButton sx={{ pl: 4 }} className={stylesProducts.listSubItem}>
-                                    <ListItemText>
-                                        <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Rotary Screw</span>
-                                    </ListItemText>
-                                </ListItemButton>
-                            </Link>
-                            <Link href='/reciprocating'>
-                                <ListItemButton sx={{ pl: 4 }} className={stylesProducts.listSubItem}>
-                                    <ListItemText>
-                                        <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Reciprocating</span>
-                                    </ListItemText>
-                                </ListItemButton>
-                            </Link>
-                            <Link href='/oilfree'>
-                                <ListItemButton sx={{ pl: 4 }} className={stylesProducts.listSubItem}>
-                                    <ListItemText>
-                                        <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Oil Free</span>
-                                    </ListItemText>
-                                </ListItemButton>
-                            </Link>
-                            <Link href='/portable'>
-                                <ListItemButton sx={{ pl: 4 }} className={stylesProducts.listSubItem}>
-                                    <ListItemText>
-                                        <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Portable</span>
-                                    </ListItemText>
-                                </ListItemButton>
-                            </Link>
-                        </List>
-                    </Collapse>
-                    <Link href='/construction'>
-                        <ListItemButton className={stylesProducts.listItem}>
+            <div className={styles.container}>
+
+                <div className={isMobile ? stylesProducts.listM : stylesProducts.list}>
+                    <List
+                        sx={{ width: '100%', bgcolor: 'background.paper' }}
+                        component="nav"
+                        aria-labelledby="nested-list-subheader"
+                        >
+                        <ListItemButton onClick={handleClick} className={stylesProducts.listItem}>
                             <ListItemText>
-                                <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Construction Equipment</span>
+                                <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Air Compressors</span>
                             </ListItemText>
+                            {open ? <ExpandLess /> : <ExpandMore />}
                         </ListItemButton>
-                    </Link>
-                    <Link href='/solutions'>
-                        <ListItemButton className={stylesProducts.listItem}>
-                            <ListItemText>
-                                <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Quality Air Solutions</span>
-                            </ListItemText>
-                        </ListItemButton>
-                    </Link>
-                    <Link href='/filters'>
-                        <ListItemButton className={stylesProducts.listItem}>
-                            <ListItemText>
-                                <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Alternate Inline Air Filter Elements</span>
-                            </ListItemText>
-                        </ListItemButton>
-                    </Link>
-                    <Link href='/tools'>
-                        <ListItemButton className={stylesProducts.listItem}>
-                            <ListItemText>
-                                <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>CP Air Tools</span>
-                            </ListItemText>
-                        </ListItemButton>
-                    </Link>
-                </List>
-                {isMobile ? null : <Footer />}
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                <Link href='/rotary'>
+                                    <ListItemButton sx={{ pl: 4 }} className={stylesProducts.listSubItem}>
+                                        <ListItemText>
+                                            <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Rotary Screw</span>
+                                        </ListItemText>
+                                    </ListItemButton>
+                                </Link>
+                                <Link href='/reciprocating'>
+                                    <ListItemButton sx={{ pl: 4 }} className={stylesProducts.listSubItem}>
+                                        <ListItemText>
+                                            <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Reciprocating</span>
+                                        </ListItemText>
+                                    </ListItemButton>
+                                </Link>
+                                <Link href='/oilfree'>
+                                    <ListItemButton sx={{ pl: 4 }} className={stylesProducts.listSubItem}>
+                                        <ListItemText>
+                                            <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Oil Free</span>
+                                        </ListItemText>
+                                    </ListItemButton>
+                                </Link>
+                                <Link href='/portable'>
+                                    <ListItemButton sx={{ pl: 4 }} className={stylesProducts.listSubItem}>
+                                        <ListItemText>
+                                            <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Portable</span>
+                                        </ListItemText>
+                                    </ListItemButton>
+                                </Link>
+                            </List>
+                        </Collapse>
+                        <Link href='/construction'>
+                            <ListItemButton className={stylesProducts.listItem}>
+                                <ListItemText>
+                                    <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Construction Equipment</span>
+                                </ListItemText>
+                            </ListItemButton>
+                        </Link>
+                        <Link href='/solutions'>
+                            <ListItemButton className={stylesProducts.listItem}>
+                                <ListItemText>
+                                    <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Quality Air Solutions</span>
+                                </ListItemText>
+                            </ListItemButton>
+                        </Link>
+                        <Link href='/filters'>
+                            <ListItemButton className={stylesProducts.listItem}>
+                                <ListItemText>
+                                    <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>Alternate Inline Air Filter Elements</span>
+                                </ListItemText>
+                            </ListItemButton>
+                        </Link>
+                        <Link href='/tools'>
+                            <ListItemButton className={stylesProducts.listItem}>
+                                <ListItemText>
+                                    <span className={isMobile ? stylesProducts.listItemTextM : stylesProducts.listItemText}>CP Air Tools</span>
+                                </ListItemText>
+                            </ListItemButton>
+                        </Link>
+                    </List>
+                    {isMobile ? null : <Footer />}
+                </div>
             </div>
         </div>
     )

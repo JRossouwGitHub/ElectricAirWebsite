95adb8ad21379ffe5f4e1a80b4ffac921b7f4ba8
JRossouwGitHub <jrossouw.work@gmail.com>
2023-09-27 19:57:32 +1300

    Add analytics

--- components/Meta.js
+++ components/Meta.js
@@ -10,6 +10,7 @@ const Meta = ({title, description}) => {
             <meta name="description" content={description} />
             <link rel="shortcut icon" href='/EA favicon.ico' />
             <title>Electric Air | {title}</title>
+            <script type="text/javascript" src="https://analytics.palmtop.co.nz/js/bundle/analytics.min.1.0.0.js"></script>
         </Head>
     )
 }
